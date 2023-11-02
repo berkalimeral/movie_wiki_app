ab16c5a3efc6148de05bba296e7278e26d2264a1
salihkaankoc <68756805+berkalimeral@users.noreply.github.com>
2023-11-02 10:35:26 +0300

    build service

--- lib/products/service/movie_service.dart
+++ lib/products/service/movie_service.dart
@@ -1,13 +1,21 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:http/http.dart' as https;
-import 'package:uni_society_app/core/constants/api_const.dart';
-import 'package:uni_society_app/products/network/network_values.dart';
+import 'package:uni_society_app/products/models/now_playing_model.dart';
+
+import '../../core/constants/api_const.dart';
+import '../models/popular_movie_model.dart';
+import '../models/trend_movie_model.dart';
+import '../network/network_values.dart';
 
 class MovieService {
-  Future<void> getNowPlayingMovies() async {
-    https.get(
+  Future<List<NowPlays>?> getNowPlayingMovies() async {
+    var response = await https.get(
         Uri(
             scheme: 'https',
-            host: nowPlayingMovie,
+            host: apiHost,
             path: EndPointsMovie.nowPlaying,
             queryParameters: {
               'language': 'tr-TR',
@@ -16,5 +24,64 @@ class MovieService {
         headers: {
           'Authorization': apiReadAccessToken,
         });
+    if (response.statusCode == HttpStatus.ok) {
+      var responseBody = jsonDecode(response.body);
+      NowPlayingModel nowPlaying = NowPlayingModel.fromJson(responseBody);
+      return nowPlaying.results;
+    } else {
+      log(
+        'Something is wrong in getNowPlaying',
+      );
+      return null;
+    }
+  }
+
+  Future<List<Populars>?> getPopularMovies() async {
+    var response = await https.get(
+        Uri(
+            scheme: 'https',
+            host: apiHost,
+            path: EndPointsMovie.popular,
+            queryParameters: {
+              'language': 'tr-TR',
+              'page': '1',
+            }),
+        headers: {
+          'Authorization': apiReadAccessToken,
+        });
+    if (response.statusCode == HttpStatus.ok) {
+      var responseBody = jsonDecode(response.body);
+      PopularMovieModel popular = PopularMovieModel.fromJson(responseBody);
+      return popular.results;
+    } else {
+      log(
+        'Something is wrong in getNowPlaying',
+      );
+      return null;
+    }
+  }
+
+  Future<List<Trends>?> getTrendingMovies() async {
+    var response = await https.get(
+        Uri(
+            scheme: 'https',
+            host: apiHost,
+            path: EndPointsMovie.trending,
+            queryParameters: {
+              'language': 'tr-TR',
+            }),
+        headers: {
+          'Authorization': apiReadAccessToken,
+        });
+    if (response.statusCode == HttpStatus.ok) {
+      var responseBody = jsonDecode(response.body);
+      TrendMovieModel trending = TrendMovieModel.fromJson(responseBody);
+      return trending.results;
+    } else {
+      log(
+        'Something is wrong in getNowPlaying',
+      );
+      return null;
+    }
   }
 }
