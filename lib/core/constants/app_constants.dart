fb8eb0221511d01e9fb71050526d77a63504e911
salihkaankoc <68756805+berkalimeral@users.noreply.github.com>
2023-11-03 10:00:46 +0300

    series title string

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,9 @@
-//App Title Constants
+//App Movie Title Constants
 const String nowPlaying = 'Now Playing';
 const String popular = 'Popular Movies';
 const String trendMovie = 'Trending Movies';
+
+//App Series Title Constants
+const String topRated = 'Top Rated Series';
+const String popularSeries = 'Popular Series';
+const String trendSeries = 'Trending Series';
